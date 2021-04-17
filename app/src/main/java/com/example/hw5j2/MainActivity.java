868cd3b79f0362b0ec5ac7b445148619f047980a
argen23 <argenomarbekov192@gmail.com>
2021-04-17 17:21:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hw5j2/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.hw5j2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+private Button btn ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.EDTXT);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+                Toast toast = Toast.makeText(getApplicationContext(),"ok" , Toast.LENGTH_SHORT);
+                toast.setMargin(50,50);
+                toast.show();
+            }
+    });
+}
+}
